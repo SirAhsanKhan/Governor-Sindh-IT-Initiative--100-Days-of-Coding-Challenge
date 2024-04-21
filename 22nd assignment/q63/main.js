cd00f77aa84a4841dbe10efe0d819ee8ad6362e2
Ahsan Khan <afzalahsankhan283@gmail.com>
2024-04-22 00:06:18 +0500

    22nd asssignment

---
+++ 22nd assignment/q63/main.js
@@ -0,0 +1,4 @@
+function combinationOfStringAndNumber(text, number) {
+    return text + number;
+}
+console.log(combinationOfStringAndNumber("class:", 10));
---
+++ 22nd assignment/q63/main.ts
@@ -0,0 +1,5 @@
+function combinationOfStringAndNumber(text: string,number: number) : string {
+    return text + number
+
+}
+console.log(combinationOfStringAndNumber("class:",10));
