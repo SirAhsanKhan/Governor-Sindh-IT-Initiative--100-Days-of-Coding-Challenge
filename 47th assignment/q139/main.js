7e2ad9476102b9f1ef3e9923ba2a430f802cc803
Ahsan Khan <afzalahsankhan283@gmail.com>
2024-05-05 23:19:11 +0500

    47th assignment done

---
+++ 47th assignment/q139/main.js
@@ -0,0 +1,7 @@
+var count = 10;
+if (count > 0) {
+    console.log("count is greater than 0");
+}
+function add(a, b) {
+    return a + b;
+}
---
+++ 47th assignment/q139/main.ts
@@ -0,0 +1,9 @@
+let count =10 
+
+if(count > 0) {
+    console.log("count is greater than 0")
+}
+
+function add ( a, b){
+    return a +  b
+}
